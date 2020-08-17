218299cdbfe02082f858b7169652dfd258df6142
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-17 16:29:48 -0700

    read working

--- step3.js
+++ step3.js
@@ -1,42 +1,70 @@
 const process = require("process");
 const fs = require("fs");
 const axios = require("axios");
-let path = process.argv[2];
+let path = null;
+let output = null;
+let w = null;
+let p = process.argv;
 
-// if path is a text file, log file content
-const cat = (path) => {
-  fs.readFile(path, "utf8", function(err, data) {
+// if path is a text file, log file content or write to file
+const cat = () => {
+  fs.readFile(path, "utf8", function (err, data) {
     if (err) {
       console.log(err);
       process.exit(1);
     }
-    console.log(data);
-  });
+    if (w === null) {
+      console.log(data);
+    } else if (w !== null) {
+      fs.writeFile(output, data, "utf8", function (err) {
+        if (err) {
+          console.error(`Error: ${err}`);
+          process.exit(1);
+        }
+      });
+    }
+  })
 };
 
-// if path is a URL, log URL content
+// if path is a URL, log URL content or write to file
 const webCat = async (path) => {
-  console.log(path);
-  try {
-    const res = await axios.get(path);
-    console.log(res.data);
-  } catch (err) {
-    console.error(`Error with path: ${path}: ${err}`);
-    process.exit(1);
-  }
+
+    try {
+      const res = await axios.get(path);
+      if (w === null) {
+        console.log(res.data);
+      } else {
+        fs.writeFile(output, res.data, 'utf8', function (err) {
+          if (err) {
+            console.error(`Error with path: ${path}: ${err}`);
+            process.exit(1);
+          } 
+        })
+      }
+    } catch {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
 };
 
 // check if path is a URL object or a file string
-const checkType = (path) => {
-    try {
-        const newUrl = new URL(
-        path
-        );
-        webCat(newUrl.href);
-    } catch {
-        console.log('file')
-        cat(path)
-    }
+const checkType = () => {
+  try {
+    const newUrl = new URL(path);
+    webCat(newUrl.href);
+  } catch {
+    cat();
+  }
+};
+
+// check for oupout value using length of process.argv
+if (p.length === 3) {
+  path = process.argv[2];
+  checkType();
+} else {
+  path = process.argv[4];
+  output = process.argv[3];
+  w = process.argv[2];
+  checkType();
 }
 
-checkType(path)
