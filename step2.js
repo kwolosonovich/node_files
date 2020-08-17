e85f38dff59b5b26328c361781b16156cf64d331
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-17 12:39:07 -0700

    added path var to object logic

--- step2.js
+++ step2.js
@@ -4,8 +4,9 @@ const axios = require("axios");
 const newUrl = new URL(
   "http://curric.rithmschool.com/springboard/exercises/node-files/"
 );
+let path = process.argv[2];
 
-
+// if path is a text file, log file content
 const cat = () => {
   fs.readFile("./one.txt", "utf8", function (err, data) {
     if (err) {
@@ -15,10 +16,8 @@ const cat = () => {
     console.log(data);
   });
 };
-
-cat();
-
-
+ 
+// if path is a URL, log URL content
 const webCat = async (path) => {
     console.log(path)
     try {
@@ -30,7 +29,8 @@ const webCat = async (path) => {
     }
 }
 
-if (typeof(newUrl) === 'object') {
+// check if path is a URL object or a file string
+if (typeof(path) === 'object') {
     webCat(newUrl.href)
 } else {
     cat()
