e02333b3dac6a729e72808a05418e266f6da935e
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-17 11:34:47 -0700

    implemented step1 func

--- step1.js
+++ step1.js
@@ -1,10 +1,14 @@
 const process = require("process");
 const fs = require("fs");
 
-fs.readFile('./one.txt', 'utf8', function(err, data) {
-    if (err) {
-        console.log(err)
-        process.exit(1)
-    }
-    console.log(data)
-})
+const cat = () => {
+    fs.readFile('./one.txt', 'utf8', function(err, data) {
+        if (err) {
+            console.log(err)
+            process.exit(1)
+        }
+        console.log(data)
+    })
+}
+
+cat()
